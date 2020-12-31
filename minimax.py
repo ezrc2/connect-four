5e487b9e6daee0838ab72803f24af9289b7e1267
ezrc2 <ezchen2556@gmail.com>
2020-12-31 14:06:52 -0800

    Add minimax class

---
+++ minimax.py
@@ -0,0 +1,12 @@
+import math
+
+class Minimax():
+
+    def __init__(self):
+        self.depth = 4
+
+    def minimax(self, position, depth, alpha, beta, maximizing):
+        pass
+
+    def evaluate_board(self, board):
+        pass
