b039498cb535ace63b8fa04ebeeeabbd44ad7be7
ezrc2 <ezchen2556@gmail.com>
2020-12-31 22:38:08 -0800

    Add row on top to drop pieces down

--- game.py
+++ game.py
@@ -20,15 +20,13 @@ LINE_WIDTH = 3
 SQUARE_SIZE = 100
 CIRCLE_RADIUS = 40
 
-TEXT_LOCATION = (150, 175)
-
 class ConnectFour:
 
     def __init__(self):
         pygame.init()
         pygame.display.init()
         pygame.font.init()
-        self.display = pygame.display.set_mode((COLUMNS * SQUARE_SIZE, ROWS * SQUARE_SIZE))
+        self.display = pygame.display.set_mode((COLUMNS * SQUARE_SIZE, (ROWS + 1) * SQUARE_SIZE))
         pygame.display.set_caption("Connect Four")
         self.font = pygame.font.SysFont("Consolas", 90)
         
@@ -43,20 +41,23 @@ class ConnectFour:
         while not game_over:
             self.draw_board()
 
-            if self.check_win(HUMAN):
-                text = self.font.render("You win!", True, BLACK)
-                self.display.blit(text, TEXT_LOCATION)
-                game_over = True
-            elif self.check_win(COMPUTER):
-                text = self.font.render("You lose!", True, BLACK)
-                self.display.blit(text, TEXT_LOCATION)
+            if self.won_game(HUMAN) or self.won_game(COMPUTER):
+                if self.won_game(HUMAN):
+                    text = self.font.render("You win!", True, BLACK)
+                else:
+                    text = self.font.render("You lose!", True, BLACK)
+                text_rect = text.get_rect(center=(SQUARE_SIZE * COLUMNS / 2, SQUARE_SIZE / 2))
+                self.display.blit(text, text_rect)
                 game_over = True
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    self.handle_mouse_click(event)
+                    self.handle_mouse_click(event.pos[0])
+
+            if not game_over:
+                self.draw_next_piece(pygame.mouse.get_pos()[0])
 
             pygame.display.update()
             clock.tick(60)
@@ -68,11 +69,12 @@ class ConnectFour:
                         sys.exit()
 
     def draw_board(self):
-        pygame.draw.rect(self.display, BLUE, (0, 0, COLUMNS * SQUARE_SIZE, ROWS * SQUARE_SIZE))
+        pygame.draw.rect(self.display, BLUE, (0, SQUARE_SIZE, COLUMNS * SQUARE_SIZE, (ROWS + 1) * SQUARE_SIZE))
+        pygame.draw.rect(self.display, WHITE, (0, 0, COLUMNS * SQUARE_SIZE, SQUARE_SIZE))
 
         for i in range(ROWS):
             for j in range(COLUMNS):
-                center = ((j  +  0.5) * SQUARE_SIZE, (i  +  0.5) * SQUARE_SIZE)
+                center = ((j  +  0.5) * SQUARE_SIZE, (i  +  1.5) * SQUARE_SIZE)
                 if self.board[i][j] == 0:
                     color = WHITE
                 elif self.board[i][j] == HUMAN:
@@ -82,26 +84,27 @@ class ConnectFour:
                 pygame.draw.circle(self.display, color, center, CIRCLE_RADIUS)
                 pygame.draw.circle(self.display, BLACK, center, CIRCLE_RADIUS, LINE_WIDTH)
 
+    def draw_next_piece(self, mouse_x):
+        column = int(mouse_x / SQUARE_SIZE)
+        center = ((column + 0.5) * SQUARE_SIZE, SQUARE_SIZE / 2)
+        pygame.draw.circle(self.display, RED, center, CIRCLE_RADIUS)
+
     def drop_piece(self, column, player):
         for i in range(ROWS - 1, -1, -1):
             if self.board[i][column] == EMPTY:
                 self.board[i][column] = player
                 break
 
-    def handle_mouse_click(self, event):
-        mouse_x = event.pos[0]
-        for j in range(COLUMNS):
-            if j * SQUARE_SIZE <= mouse_x < (j  +  1) * SQUARE_SIZE:
-                if self.human_turn:
-                    self.drop_piece(j, HUMAN)
-                    self.human_turn = False
-                    break
-                else:
-                    self.drop_piece(j, COMPUTER)
-                    self.human_turn = True
-                    break
+    def handle_mouse_click(self, mouse_x):
+        column = int(mouse_x / SQUARE_SIZE)
+        if self.human_turn:
+            self.drop_piece(column, HUMAN)
+            self.human_turn = False
+        else:
+            self.drop_piece(column, COMPUTER)
+            self.human_turn = True
 
-    def check_win(self, player):
+    def won_game(self, player):
         # Check rows
         for row in range(ROWS):
             for col in range(COLUMNS - 3):
