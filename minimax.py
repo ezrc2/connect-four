cd5b12e4d69ac8e731315baffa41586609396f7f
ezrc2 <ezchen2556@gmail.com>
2021-01-02 00:04:59 -0800

    Add function definitions and part of minimax

--- minimax.py
+++ minimax.py
@@ -1,12 +1,43 @@
 import math
 
+EMPTY = 0
+HUMAN_PLAYER = 1
+AI_PLAYER = 2
+
 class Minimax():
 
     def __init__(self):
         self.depth = 4
 
-    def minimax(self, position, depth, alpha, beta, maximizing):
-        pass
+    def find_move(self, board):
+        move, score = self.minimax(board, self.depth, True)
+        return move
 
-    def evaluate_board(self, board):
-        pass
+    def get_heuristic(self, board):
+        heuristic = 0
+
+ 
+
+        return heuristic
+
+
+    def is_terminal(self, board):
+        return board.won_game(HUMAN_PLAYER) or board.won_game(AI_PLAYER) or board.is_full()
+
+    def minimax(self, board, depth, maximizing):
+        if depth == 0:
+            return -1, self.get_heuristic(board)
+        elif self.is_terminal(board):
+            if board.won_game(AI_PLAYER):
+                return -1, math.inf
+            elif board.won_game(HUMAN_PLAYER):
+                return -1, -math.inf
+            else:
+                return -1, 0
+
+        if maximizing:
+            pass
+
+
+
+        return 0
