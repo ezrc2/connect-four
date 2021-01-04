d0c36a37f118f3ce7260c399293f031dec003d1e
ezrc2 <ezchen2556@gmail.com>
2021-01-03 20:53:22 -0800

    Implement minimax function

--- minimax.py
+++ minimax.py
@@ -6,38 +6,47 @@ AI_PLAYER = 2
 
 class Minimax():
 
-    def __init__(self):
-        self.depth = 4
+    def __init__(self, depth):
+        self.depth = depth
 
     def find_move(self, board):
-        move, score = self.minimax(board, self.depth, True)
+        move, j = self.minimax(board, self.depth, True)
+        print (move, j)
         return move
 
     def get_heuristic(self, board):
         heuristic = 0
 
- 
+        
 
         return heuristic
 
 
-    def is_terminal(self, board):
-        return board.won_game(HUMAN_PLAYER) or board.won_game(AI_PLAYER) or board.is_full()
-
     def minimax(self, board, depth, maximizing):
         if depth == 0:
-            return -1, self.get_heuristic(board)
-        elif self.is_terminal(board):
-            if board.won_game(AI_PLAYER):
-                return -1, math.inf
-            elif board.won_game(HUMAN_PLAYER):
-                return -1, -math.inf
-            else:
-                return -1, 0
+            return (-1, self.get_heuristic(board))
+        if board.won_game(AI_PLAYER):
+            return (-1, math.inf)
+        if board.won_game(HUMAN_PLAYER):
+            return (-1, -math.inf)
+        if board.is_full():
+            return (-1, 0)
 
         if maximizing:
-            pass
+            best_move = -1
+            best_score = -math.inf
+            for move, child in board.get_children(AI_PLAYER):
+                score = self.minimax(child, depth - 1, False)[1]
+                if score > best_score:
+                    best_score = score
+                    best_move = move
+        else:
+            best_move = -1
+            best_score = math.inf
+            for move, child in board.get_children(HUMAN_PLAYER):
+                score = self.minimax(child, depth - 1, True)[1]
+                if score > best_score:
+                    best_score = score
+                    best_move = move
 
-
-
-        return 0
+        return best_move, best_score
