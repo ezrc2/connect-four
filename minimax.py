5db0747f5a9a486ab63499188724397794d48afc
ezrc2 <ezchen2556@gmail.com>
2021-01-05 12:40:25 -0800

    Add alpha-beta pruning

--- minimax.py
+++ minimax.py
@@ -4,13 +4,16 @@ EMPTY = 0
 HUMAN_PLAYER = 1
 AI_PLAYER = 2
 
+INITIAL_ALPHA = -math.inf
+INITIAL_BETA = math.inf
+
 class Minimax():
 
     def __init__(self, depth):
         self.depth = depth
 
     def find_move(self, board):
-        move = self.minimax(board, self.depth, True)[0]
+        move = self.minimax(board, self.depth, INITIAL_ALPHA, INITIAL_BETA, True)[0]
         return move
 
     def get_heuristic(self, board):
@@ -18,13 +21,13 @@ class Minimax():
 
         for row in board.board:
             for j in range(board.columns - 3):
-                lst = row[j:j+4]
+                lst = row[j : j + 4]
                 heuristic += self.evaluate_streak(lst)
 
         for j in range(board.columns):
             column = [row[j] for row in board.board]
             for i in range(board.rows - 3):
-                lst = column[i:i+4]
+                lst = column[i : i + 4]
                 heuristic += self.evaluate_streak(lst)
 
         for i in range(board.rows - 3):
@@ -43,21 +46,21 @@ class Minimax():
         heuristic = 0
 
         if streak.count(AI_PLAYER) == 4:
-            heuristic += 1000
+            heuristic += math.inf
         elif streak.count(AI_PLAYER) == 3 and streak.count(EMPTY) == 1:
-            heuristic += 50
+            heuristic += 5
         elif streak.count(AI_PLAYER) == streak.count(EMPTY) == 2:
-            heuristic += 10
+            heuristic += 1
         elif streak.count(HUMAN_PLAYER) == streak.count(EMPTY) == 2:
-            heuristic -= 10
+            heuristic -= 1
         elif streak.count(HUMAN_PLAYER) == 3 and streak.count(EMPTY) == 1:
-            heuristic -= 50
+            heuristic -= 5
         elif streak.count(HUMAN_PLAYER) == 4:
-            heuristic -= 1000
+            heuristic -= 10000
 
         return heuristic
 
-    def minimax(self, board, depth, maximizing):
+    def minimax(self, board, depth, alpha, beta, maximizing):
         if board.won_game(AI_PLAYER):
             return (-1, math.inf)
         if board.won_game(HUMAN_PLAYER):
@@ -67,22 +70,28 @@ class Minimax():
         if depth == 0:
             return (-1, self.get_heuristic(board))
 
+        valid_columns = board.get_valid_columns()
+        best_move = valid_columns[len(valid_columns) // 2]
+
         if maximizing:
-            valid_columns = board.get_valid_columns()
-            best_move = valid_columns[len(valid_columns) // 2]
             best_score = -math.inf
             for move, child in board.get_children(AI_PLAYER):
-                score = self.minimax(child, depth - 1, False)[1]
+                score = self.minimax(child, depth - 1, alpha, beta, False)[1]
                 if score > best_score:
                     best_score = score
                     best_move = move
+                alpha = max(alpha, score)
+                if alpha >= beta:
+                    break
         else:
-            best_move = -1
             best_score = math.inf
             for move, child in board.get_children(HUMAN_PLAYER):
-                score = self.minimax(child, depth - 1, True)[1]
+                score = self.minimax(child, depth - 1, alpha, beta, True)[1]
                 if score < best_score:
                     best_score = score
                     best_move = move
+                alpha = min(alpha, score)
+                if alpha >= beta:
+                    break
 
         return best_move, best_score
